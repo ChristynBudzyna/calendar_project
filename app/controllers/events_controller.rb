1bc9fd4a9193f3cacdbcf9c646ff3ace6f8f2213
Christyn Budzyna <cbudzyna@gmail.com>
2016-07-28 17:10:45 -0400

    Add new and create methods to Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,25 @@ def show
   @event = Event.find_by(id: params[:id])
 end
 
+def new
+  @event = Event.new
+end
+
+def create
+  @event = Event.new(event_params)
+   if @event.save
+      performances.each do |performance|
+        @event.performances << Performance.create(performance_params)
+      end
+    end
+end
+
+ private
+    def event_params
+      params.require( :event ).permit( :title, :writers, :venue, :performers, :description, :price_range, :ticket_link, :video_link)
+    end
+
+    def performance_params
+      params.require( :performance ).permit(:start_time, :end_time, :date, :event_id)
+
 end
